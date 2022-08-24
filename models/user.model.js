8de447ae885cae679d9c2bd3e1c6d7e1fc091857
Rene0115 <enere0115@gmail.com>
2022-08-24 09:21:16 +0100

    user controller setup

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
-export const User = mongoose.model('User', userSchema);
+const userModel = mongoose.model('User', userSchema);
 
-export default {
-  User
-};
+export default userModel;
