1dbda2887930c9ad87e8703543491d9da8347fd0
Rene0115 <enere0115@gmail.com>
2023-04-06 11:12:29 +0100

    updated multer file

--- config/multer.config.js
+++ config/multer.config.js
@@ -2,18 +2,14 @@ import multer from "multer";
 import path from "path";
 
 const store = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, "uploads");
-    },
-    filefilter: (req, file, cb) => {
-      const ext = path.extname(file.originalName);
-      if (ext !== "mp4" || ext !== ".mkv") {
-        cb(new Error("File type is not supported"), false);
-      }
-      cb(null, true);
+  storage: multer.diskStorage({}),
+  filefilter: (req, file, cb) => {
+    const ext = path.extname(file.originalName);
+    if (ext !== ".mp4" || ext !== ".mkv" ) {
+      cb(new Error("File type is not supported"), false);
+      return;
     }
-  })
+    cb(null, true);
+  },
 });
-
 export default store;
