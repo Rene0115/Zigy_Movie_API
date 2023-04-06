606f363e4d59a7fc4648ae1998f6c8498c387ef3
Rene0115 <enere0115@gmail.com>
2023-04-06 11:01:19 +0100

    added morgan

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,10 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 import bodyParser from "body-parser";
 import cors from "cors";
 import router from "../routes/index.routes.js";
 import errorHandler from "./error.middleware.js";
 
 const middleware = (app) => {
+  app.use(morgan("dev"));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: true }));
